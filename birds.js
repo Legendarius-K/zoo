0bb94faeacb07855ec9faa27e1a4cc1fe2e0b49d
Shada Baz <shada.bazger@edu.cmeducations.se>
2024-01-16 12:30:45 +0100

    add birds.html and birds.js

---
+++ birds.js
@@ -0,0 +1,53 @@
+$(() => {
+    
+    function Animal(species, lifespan, group, food, description, length, weight, found) {
+        this.species = species,
+        this.lifespan = lifespan,
+        this.group = group,
+        this.food = food,
+        this.description = description,
+        this.length = length,
+        this.weight = weight,
+        this.found = found,
+        this.createInfoElement = function () {
+            $(".animal-info-container").append(`
+                <div class="animal-info ${this.species}">
+                    <h3>${this.species}</h3>
+                    <p>${this.lifespan}</p>
+                    <p>${this.group}</p>
+                    <p>${this.food}</p>
+                    <p>${this.description}</p>
+                    <p>${this.length}</p>
+                    <p>${this.weight}</p>
+                    <p>${this.found}</p>
+                </div>
+            `)
+        }
+    };
+
+    let cassowary = new Animal("Cassowary", "20 years", "bird", "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida.", "1,7 m", "44 kg","Queensland");
+    let darius = new Animal("Darius");
+    let ant = new Animal("Ant");
+
+    let animalArray = [echidna, darius, ant];
+
+    console.log(animalArray);
+    $(animalArray).each((index, animal) => animal.createInfoElement());
+    
+    const populateSidebar = () => {
+        $(animalArray).each((index, animal) => {
+            $(".side-bar").append(`<h4 class="side-bar-animal">${animal.species}</h4>`)
+       })
+    };
+
+    populateSidebar();
+
+    $(`.side-bar-animal`).on("click", e => {
+        let clickedSpecies = $(e.target).text();
+        $(`.animal-info.${clickedSpecies}`).toggle();
+        $(`.animal-info.${clickedSpecies}`).siblings().fadeOut(1);
+    })
+    
+    
+   
+});
