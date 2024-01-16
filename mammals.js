e3ecc4b866a9febc197c7116ba95598b7b095c53
Darius K <darius.kaya@gmail.com>
2024-01-16 11:43:35 +0100

    add sidebar click functionality

--- mammals.js
+++ mammals.js
@@ -10,24 +10,48 @@ $(() => {
         this.weight = weight,
         this.found = found,
         this.createInfoElement = function () {
-            $(".mammal-info").append(`
-                <h3>${this.species}</h3>
-                <p>${this.lifespan}</p>
-                <p>${this.group}</p>
-                <p>${this.food}</p>
-                <p>${this.description}</p>
-                <p>${this.length}</p>
-                <p>${this.weight}</p>
-                <p>${this.found}</p>
+            $(".animal-info-container").append(`
+                <div class="animal-info ${this.species}">
+                    <h3>${this.species}</h3>
+                    <p>${this.lifespan}</p>
+                    <p>${this.group}</p>
+                    <p>${this.food}</p>
+                    <p>${this.description}</p>
+                    <p>${this.length}</p>
+                    <p>${this.weight}</p>
+                    <p>${this.found}</p>
+                </div>
             `)
         }
     };
 
     let echidna = new Animal("Echidna", "50 years", "mammals", "Insects such as ants and termites, beetle larvae and worms", "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.", "76 cm", "10 kg", "Throughout Australia");
+    let darius = new Animal("Darius");
+    let ant = new Animal("Ant");
 
-    let animalArray = [echidna];
+    let animalArray = [echidna, darius, ant];
 
-console.log(animalArray);
+    console.log(animalArray);
     $(animalArray).each((index, animal) => animal.createInfoElement());
     
-})
+    const populateSidebar = () => {
+        $(animalArray).each((index, animal) => {
+            $(".side-bar").append(`<h4 class="side-bar-animal">${animal.species}</h4>`)
+       })
+    };
+
+    populateSidebar();
+
+    $(`.side-bar-animal`).on("click", e => {
+        let clickedSpecies = $(e.target).text();
+        $(`.animal-info.${clickedSpecies}`).toggle();
+        $(`.animal-info.${clickedSpecies}`).siblings().fadeOut(1);
+    })
+    
+    
+   
+});
+   
+
+           
+
