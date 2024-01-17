216000eab3cd840c58d7f573aa9d34e50ca846ec
Darius K <darius.kaya@gmail.com>
2024-01-17 16:04:19 +0100

    fix welcome message bug

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ $(() => {
         $(".main-content").append(`
             <div class=welcome-message>
             <h3>Welcome to The Fantastic Zoo</h3> 
-            <p>Our popular zoo has recently got some new residents! We are happy to welcome a range of Australian species such as the ${mammalArray[0]}, the ${reptileArray[0]} and the ${birdArray[0]}.</p>
+            <p>Our popular zoo has recently got some new residents! We are happy to welcome a range of Australian species such as the ${mammalArray[0].species}, the ${reptileArray[0].species} and the ${birdArray[0].species}.</p>
             <p>They will stay with us as long as they need some extra care before being released back into the nature that is their beloved home.</p>
             <p>Check out the brief summary of all of our new friends in the menu on the left. For the enthusiastic animal nerd there is even more to read if you go to the detalied animal pages found in the menu above. And of course we hope you visit us to say hi to our new friends.</p>
             <p>// The dedicated team @ The Fantastic Zoo</p>
