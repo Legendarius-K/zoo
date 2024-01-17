277bd5c2830ffb3a1191dffce7dabd148346cd6d
Shada Baz <shada.bazger@edu.cmeducations.se>
2024-01-17 12:06:32 +0100

    add bird.css

--- birds.js
+++ birds.js
@@ -26,10 +26,10 @@ $(() => {
     };
 
     let cassowary = new Animal("Cassowary", "20 years", "bird", "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida.", "1,7 m", "44 kg","Queensland");
-    let darius = new Animal("Darius");
-    let ant = new Animal("Ant");
+    let songbird = new Animal("Songbird");
+    let parrot = new Animal("Parrot");
 
-    let animalArray = [cassowary, darius, ant];
+    let animalArray = [cassowary, songbird, parrot];
 
     console.log(animalArray);
     $(animalArray).each((index, animal) => animal.createInfoElement());
